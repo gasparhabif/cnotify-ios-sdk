f8a7f025801ecb0878a238bdbb8e91328511aeeb
Gaspi Habif <gasparhabif@gmail.com>
2024-09-17 13:28:32 -0300

    feat: added gitignore and swift package

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "CNotifySDK",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "CNotifySDK",
+            targets: ["CNotifySDK"]),
+    ],
+    dependencies: [
+        // Add any dependencies here, like Firebase
+        .package(url: "https://github.com/firebase/firebase-ios-sdk.git", from: "8.0.0")
+    ],
+    targets: [
+        .target(
+            name: "CNotifySDK",
+            dependencies: ["Firebase"]),
+        .testTarget(
+            name: "CNotifySDKTests",
+            dependencies: ["CNotifySDK"]),
+    ]
+)
