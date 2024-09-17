965544ad999d84d96167f43aa5d8d9546495a083
Gaspi Habif <gasparhabif@gmail.com>
2024-09-17 00:38:17 -0300

    Initial Commit

---
+++ CNotifySDK/CNotifySDK.h
@@ -0,0 +1,18 @@
+//
+//  CNotifySDK.h
+//  CNotifySDK
+//
+//  Created by Gaspi Habif on 17/09/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CNotifySDK.
+FOUNDATION_EXPORT double CNotifySDKVersionNumber;
+
+//! Project version string for CNotifySDK.
+FOUNDATION_EXPORT const unsigned char CNotifySDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CNotifySDK/PublicHeader.h>
+
+
